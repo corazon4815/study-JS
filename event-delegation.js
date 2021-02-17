ff01c8bd920a12a3b5b1064127d73902b233d734
corazon4815 <mango4815@knou.ac.kr>
2021-02-17 09:24:57 +0900

    자바스크립트 동작원리 정리중

---
+++ event-delegation.js
@@ -0,0 +1,15 @@
+function A() {
+    $(".last-el").click(function(){
+        alert("last-el");
+    });
+    $(".third-cover").click(function(){
+        alert("third-cover");
+    });
+    $(".second-cover").click(function(){
+        alert("second-cover");
+    });
+    $(".first-cover").click(function(){
+        alert("first-cover");
+    });
+}
+ 
